64e99cd564999c226f68ca992397258cc6bb8d35
Ngaimanw <86390413+Ngaimanw@users.noreply.github.com>
2021-09-27 13:54:04 +0800

    todolist - update
    html && css && Js

---
+++ Ngaiman_todolist/server_csv/public/src/js/leftmenu.js
@@ -0,0 +1,7 @@
+function openSlideMeun(){
+    document.getElementById('side-menu').style.width = '300px';
+}
+
+function closeSlideMenu(){
+    document.getElementById('side-menu').style.width = '0px';
+}
